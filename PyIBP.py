cd42a293fbcb2a967a7fc548b47fb12040e58b56
David Andrzejewski <andrzeje@cs.wisc.edu>
2010-10-02 21:33:51 -0500

    add license information, revise readme, etc

--- PyIBP.py
+++ PyIBP.py
@@ -1,7 +1,25 @@
 """
+PyIBP
+
 Implements fast Gibbs sampling for the linear-Gaussian
-infinite latent feature model (IBP)
+infinite latent feature model (IBP).
+
+Copyright (C) 2009 David Andrzejewski (andrzeje@cs.wisc.edu)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 import numpy as NP
 import numpy.random as NR
 import scipy.stats as SPST
